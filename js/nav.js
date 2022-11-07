44987a12564acb080cf76a1d349f54e018d8d535
di-ch <sonya_vst@gm.gist.ac.kr>
2022-11-07 15:24:13 +0300

    scrolling

---
+++ js/nav.js
@@ -0,0 +1,12 @@
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+anchors.forEach((anchor) => {
+    anchor.addEventListener("click", (event) => {
+        event.preventDefault();
+        const blockID = anchor.getAttribute("href");
+        document.querySelector('' + blockID).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        })
+    })
+})
